d1a35ed8ed82b9a148cd4af34239e19dbc6c8f37
Matt Jones <gitcode@magisa.org>
2023-06-16 16:53:14 -0800

    GHA without dataone deps.

--- _check_redland.sh
+++ _check_redland.sh
@@ -10,6 +10,7 @@ TOP=$(pwd)
 apt list |grep librdf
 
 # Check R packages
-Rscript -e "library(dataone); devtools::session_info()" # Print the session when dataone loads
+Rscript -e "library(devtools); devtools::session_info()" # Print the session when dataone loads
+#Rscript -e "library(dataone); devtools::session_info()" # Print the session when dataone loads
 
 cd "$TOP"
