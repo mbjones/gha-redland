f8879694931a839bc28ecd1f3945ec8cf802c18a
Matt Jones <gitcode@magisa.org>
2023-06-16 17:39:23 -0800

    Cut out r-lib dependencies, use devtools instead.

--- _check_redland.sh
+++ _check_redland.sh
@@ -10,7 +10,6 @@ TOP=$(pwd)
 apt list |grep librdf
 
 # Check R packages
-Rscript -e "library(devtools); devtools::session_info()" # Print the session when dataone loads
-#Rscript -e "library(dataone); devtools::session_info()" # Print the session when dataone loads
+Rscript -e "library(dataone); devtools::session_info()" # Print the session when dataone loads
 
 cd "$TOP"
