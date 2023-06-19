3d5f0d1a8807112b65a0f033e263c98d29b7ac3f
Matt Jones <gitcode@magisa.org>
2023-06-19 10:18:06 -0800

    Remove guts of shell script.

--- _check_redland.sh
+++ _check_redland.sh
@@ -3,13 +3,4 @@
 # check that libraries installed properly
 set -e
 
-export TOP
-TOP=$(pwd)
-
-# Check apt packages
-apt list |grep librdf
-
-# Check R packages
-Rscript -e "library(dataone); devtools::session_info()" # Print the session when dataone loads
-
-cd "$TOP"
+echo "Shell script not needed, moved to GHA workflow."
